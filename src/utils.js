b6d000d049f0462b369ed483c7b4ae4488377401
Ezerous <ezerous@gmail.com>
2018-08-23 19:17:07 +0300

    Up Firebase dependency, small doc changes

--- src/utils.js
+++ src/utils.js
@@ -5,12 +5,12 @@ module.exports.hash = function (string)
     return crypto.createHash('md5').update(string).digest('hex').substring(0,12);
 };
 
+//Helper function (Firebase FCM does not accept integers as JSON values)
 module.exports.stringifyJSONIntegers = function (json)
 {
     for (let k in json)
-    {
         if (json.hasOwnProperty(k))
             json[k] = String(json[k]);
-    }
+
     return json;
 };
