c181cadb344bcf20875b83d5e057c1bc05ef01fb
Ezerous <ezerous@gmail.com>
2018-10-09 20:31:16 +0300

    Version 0.3.1

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Backend service that fetches data from [thmmy.gr](https://www.thmmy.gr/) and pus
 
 ## Usage
 
+### Development
+
 Install dependepcies using [yarn](https://yarnpkg.com/) (recommended):
 
 ```bash
@@ -31,4 +33,63 @@ or npm:
 npm start
 ```
 
-To run it continuously in the background (e.g. in production) you can use something like [pm2](https://pm2.keymetrics.io).
+### Production
+
+A quick proposed way to set up everything in production from scratch is the following (Ubuntu server):
+
+Open a root terminal to make sure you don't run into permission problems:
+```bash
+sudo -s
+```
+
+Install [nvm](https://github.com/creationix/nvm) with:
+```bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+```
+
+**Note**: If you get `nvm: command not found` after running the above, simply close your current terminal and open a new one.
+
+Install Node using nvm:
+```bash
+nvm install node
+```
+
+Install [yarn](https://yarnpkg.com/) and [pm2](https://pm2.io/) globally using npm. Also install [pm2-logrotate](https://github.com/keymetrics/pm2-logrotate):
+```bash
+npm install -g yarn pm2
+pm2 install pm2-logrotate
+```
+
+Clone Sisyphus using git:
+```bash
+git clone https://github.com/ThmmyNoLife/Sisyphus.git Sisyphus-prod
+```
+
+Create a directory name *config* where a backup of the required configuration will be kept. After that, copy the template *config.json* file from the *Sisyphus-prod/config* directory to *config*:
+```bash
+mkdir config
+cp Sisyphus-prod/config/config.json config
+```
+
+Edit the *config/config.json* file (e.g. with `nano config/config.json`), add a valid *serviceAccountKey.json* inside *config* and copy everything to *Sisyphus-prod/config* with:
+```bash
+cp -rf config Sisyphus-prod
+```
+
+Install dependencies and run Sisyphus using pm2:
+```bash
+cd Sisyphus-prod
+yarn
+NODE_ENV=production pm2 start app.js --name Sisyphus-prod
+```
+
+Configure pm2 to restart itself and the process:
+```bash
+pm2 startup ubuntu  # Sets a startup hook
+pm2 save  # Saves current process list
+```
+
+To monitor Sisyphus use:
+```bash
+pm2 monit
+```
--- src/firebase.js
+++ src/firebase.js
@@ -6,36 +6,53 @@ const serviceAccount = require('../config/' + config.firebaseServiceAccountKey);
 
 const databaseURL = config.firebaseDatabaseURL;
 
+const maxAttempts  = 10;
+const reAttemptCooldown  = 5000;
 
-module.exports.init = async function (){
+async function init(){
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
         databaseURL: databaseURL
     });
-    log.verbose('Firebase: initialized.');
-};
+    log.verbose('Firebase: Initialized.');
+}
 
-module.exports.sendForTopic = function (post){
-    const messageInfo = '(topicId, postId) =  (' + post.topicId + ', ' + post.postId +')';
+function sendForTopic(post, attempt = 1){
+    const messageInfo = '(topicId, postId)=(' + post.topicId + ', ' + post.postId +')';
     admin.messaging().sendToTopic('/topics/' + post.topicId, {data: post}, {priority: "high"})
         .then((response) => {
-            log.verbose('Firebase: successfully sent message ' + messageInfo + ' with messageId: ' +  response.messageId);
+            log.verbose('Firebase: Successfully sent message ' + messageInfo + ' with messageId: ' +  response.messageId);
         })
         .catch((error) => {
-            log.verbose('Firebase: error sending message ' + messageInfo);
+            log.verbose('Firebase: Error sending message ' + messageInfo);
             log.error(error);
+            if (attempt < maxAttempts) {
+                attempt++;
+                log.verbose('Firebase: Reattempting in ' + reAttemptCooldown/1000 +'s (attempt ' + attempt +')...');
+                setTimeout(sendForTopic, reAttemptCooldown, post, attempt);
+            }
         });
-};
+}
 
-module.exports.sendForBoard = function (post){
-    const messageInfo = '(topicId, postId, boardId) =  (' + post.topicId + ', ' + post.postId + ', ' + post.boardId + ')';
+function sendForBoard(post, attempt = 1){
+    const messageInfo = '(topicId, postId, boardId)=(' + post.topicId + ', ' + post.postId + ', ' + post.boardId + ')';
     admin.messaging().sendToTopic('/topics/b' + post.boardId, {data: post}, {priority: "high"})
         .then((response) => {
-            log.verbose('Firebase: successfully sent message ' + messageInfo + ' with messageId: ' +  response.messageId);
+            log.verbose('Firebase: Successfully sent message ' + messageInfo + ' with messageId: ' +  response.messageId);
         })
         .catch((error) => {
-            log.verbose('Firebase: error sending message ' + messageInfo);
+            log.verbose('Firebase: Error sending message ' + messageInfo);
             log.error(error);
+            if (attempt < maxAttempts) {
+                attempt++;
+                log.verbose('Firebase: Reattempting in ' + reAttemptCooldown/1000 +'s (attempt ' + attempt +')...');
+                setTimeout(sendForBoard, reAttemptCooldown, post, attempt);
+            }
         });
-};
+}
 
+module.exports = {
+    init,
+    sendForTopic,
+    sendForBoard
+};
