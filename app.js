f7716c3930b36269b68023e772c8e0ff98542bf2
Ezerous <ezerous@gmail.com>
2019-02-27 17:07:12 +0200

    Added boards that normally don't appear in unread

--- app.js
+++ app.js
@@ -16,12 +16,14 @@ const thmmy = require('thmmy');
 const getUnreadPosts = thmmy.getUnreadPosts;
 const getTopicBoards = thmmy.getTopicBoards;
 const login = thmmy.login;
+const markTopicAsUnread = thmmy.markTopicAsUnread;
 
 // Config
 const config = require('./config/config.json');
 const dataFetchCooldown = config.dataFetchCooldown;  // Cooldown before next data fetch
-const savePostsToFile = config.savePostsToFile;
+const extraBoards = config.extraBoards;
 const recentPostsLimit = config.recentPostsLimit;
+const savePostsToFile = config.savePostsToFile;
 
 const reachableCheckCooldown = 2000;
 let nIterations = 0, cookieJar, postsHash, latestPostId;
@@ -34,9 +36,13 @@ async function main() {
         await firebase.init();
         cookieJar = await login(config.thmmyUsername, config.thmmyPassword);
         let posts = await getUnreadPosts(cookieJar, { boardInfo: true, unreadLimit: recentPostsLimit });
+        if(extraBoards.length>0){
+            const extraPosts = await getUnreadPosts(cookieJar, { boardInfo: true, unreadLimit: recentPostsLimit, boards: extraBoards });
+            posts = mergePosts(posts, extraPosts);
+        }
         savePosts(posts);    // Save initial posts
         postsHash = hash(JSON.stringify(posts));
-        latestPostId = posts[0].postId;
+        latestPostId = posts.length > 0 ? posts[0].postId : -1;
         log.verbose('App: Initialization successful!');
     }
     catch (error) {
@@ -70,10 +76,53 @@ async function main() {
     }
 }
 
+function mergePosts(posts1, posts2) {
+    let posts = posts1.concat(posts2);
+    posts.sort(function(a, b) {
+        return b.timestamp - a.timestamp;
+    });
+    return (recentPostsLimit<posts.length) ? posts.slice(0,recentPostsLimit) : posts;
+}
+
 function savePosts(posts){
     firebase.saveToFirestore(posts);
     if(savePostsToFile)
         writePostsToFile(posts);
+}
+
+// For FCM messages (push notifications)
+async function pushToFirebase(newPosts) {
+    if (newPosts.length > 0) {
+        log.verbose('App: Found ' + newPosts.length + ' new post(s)!');
+        newPosts.forEach(function (post) {
+            if (post.postId > latestPostId)
+                latestPostId = post.postId;
+            stringifyJSONValues(post);
+        });
+
+        newPosts.reverse();
+
+        let newBoardPosts = [];
+        for (let i = 0; i < newPosts.length; i++) {
+            let boards = await getTopicBoards(newPosts[i].topicId, {cookieJar: cookieJar});
+            await markTopicAsUnread(newPosts[i].topicId, cookieJar);    // The line above will mark is as read
+            boards.forEach(function (board) {
+                let newBoardPost = JSON.parse(JSON.stringify(newPosts[i]));   // Deep cloning
+                newBoardPost = Object.assign(newBoardPost, board);
+                newBoardPost.boardId = newBoardPost.boardId.toString();
+                newBoardPost.boardIds = JSON.stringify(boards.map(b => b.boardId));
+                newBoardPosts.push(newBoardPost);
+            });
+        }
+
+        newPosts.forEach(function (newPost) {
+            firebase.send(newPost.topicId, newPost);
+        });
+
+        newBoardPosts.forEach(function (newBoardPost) {
+            firebase.send('b' + newBoardPost.boardId, newBoardPost);
+        });
+    }
 }
 
 async function fetch() {
@@ -81,42 +130,18 @@ async function fetch() {
     log.verbose('App: Current iteration: ' + nIterations);
     const tStart = performance.now();
     let posts = await getUnreadPosts(cookieJar, { boardInfo: true, unreadLimit: recentPostsLimit });
+    if(extraBoards.length>0){
+        const extraPosts = await getUnreadPosts(cookieJar, { boardInfo: true, unreadLimit: recentPostsLimit, boards: extraBoards });
+        posts = mergePosts(posts, extraPosts);
+    }
     if(posts && posts.length>0) {
         let currentHash = hash(JSON.stringify(posts));
         if(currentHash!==postsHash) {
             log.verbose('App: Got a new hash...');
             savePosts(posts);
             let newPosts = posts.filter(post => post.postId>latestPostId);
-            if(newPosts.length>0) {
-                log.verbose('App: Found ' + newPosts.length + ' new post(s)!');
-                newPosts.forEach(function(post) {
-                    if(post.postId>latestPostId)
-                        latestPostId = post.postId;
-                    stringifyJSONValues(post);
-                });
-
-                newPosts.reverse();
-
-                let newBoardPosts = [];
-                for(let i=0; i<newPosts.length; i++) {
-                    let boards = await getTopicBoards(newPosts[i].topicId, {cookieJar: cookieJar});
-                    boards.forEach(function(board) {
-                        let newBoardPost = JSON.parse(JSON.stringify(newPosts[i]));   // Deep cloning
-                        newBoardPost = Object.assign(newBoardPost, board);
-                        newBoardPost.boardId = newBoardPost.boardId.toString();
-                        newBoardPost.boardIds = JSON.stringify(boards.map(b => b.boardId));
-                        newBoardPosts.push(newBoardPost);
-                    });
-                }
-
-                newPosts.forEach(function(newPost) {
-                    firebase.send(newPost.topicId, newPost);
-                });
-
-                newBoardPosts.forEach(function(newBoardPost) {
-                    firebase.send('b'+ newBoardPost.boardId, newBoardPost);
-                });
-            }
+            if(newPosts.length>0)
+                pushToFirebase(newPosts);
             else
                 log.verbose('App: ...but no new posts were found.');
 
